35dddfb0f9ce01d1a38d76a812228984cbfb6aa5
Soha <sohaelsafey@yahoo.com>
2024-11-08 13:51:58 +0200

    load Data Commit

--- Classify.py
+++ Classify.py
@@ -1,6 +1,11 @@
 import pandas as pd
-
-Tweets = pd.read_csv('D:\\PHD Thesis\\2018-E-c-En-train\\2018-E-c-En-train.csv')
-Tweets.info()
-labels = list(Tweets.columns[2:])
-print(labels)
+class Classify:
+    def __init__(self):
+        return
+    def loadData(self):
+        try:
+            Tweets = pd.read_csv('2018-E-c-En-train.csv')
+            labels = list(Tweets.columns[2:])
+            return Tweets,labels
+        except Exception as e:
+            return e
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from Classify import Classify
 st.set_page_config(layout="wide")
 # Streamlit UI
 st.title("Emotion Detection")
@@ -8,6 +9,11 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button('Load Data'):
         st.write("Data Loaded")
+        c = Classify()
+        Tweets,labels = c.loadData()
+        st.write(len(Tweets),"record loaded")
+        st.write(len(labels),"labels")
+        st.write(labels)
     else:
         st.write("No Data Loaded")
 
