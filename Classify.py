7a885eb3504ae8e4039a38e431b2364e8ceff42a
Soha <sohaelsafey@yahoo.com>
2024-11-08 19:39:07 +0200

    Building, Training & Testing Commit

--- Classify.py
+++ Classify.py
@@ -17,7 +17,7 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
 class Classify:
     def __init__(self):
-        return 0
+        return
 
     def loadData(self):
         Tweets = pd.read_csv('2018-E-c-En-train.csv')
