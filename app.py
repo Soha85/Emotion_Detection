5f5d730f69ebb373f84327f5dfcc70610e3af7b1
Soha <sohaelsafey@yahoo.com>
2024-11-08 17:42:27 +0200

    Building, Training & Testing Commit

--- app.py
+++ app.py
@@ -30,13 +30,14 @@ with col1:
 
 with col2:
     st.write("**Bert + CNN Model**")
-    test_size = st.number_input("Test Size", min_value=0.1, max_value=0.5, step=0.1)
-    # if st.button("Split Data"):
-    #     train_loader,test_loader,labels_n = c.TrainPreparing(Tweets["embedding"].tolist(),labels,test_size)
-    #     st.write("Data Splitted")
-    #     model,criterion,optimizer = c.BuildModel(768,labels_n)
-    #     st.write("Model Built")
-    #     num_epochs = st.number_input("Epochs", min_value=10, max_value=50, step=5)
-    #     model = c.TrainModel(model,criterion,optimizer,num_epochs, train_loader)
-    #     st.write("Model Trained")
-    #     st.write("Result of Testing Model:", c.TestModel(model,test_loader))
+    if(len(Tweets)>0):
+        test_size = st.number_input("Test Size", min_value=0.1, max_value=0.5, step=0.1)
+        if st.button("Split Data"):
+            train_loader,test_loader,labels_n = c.TrainPreparing(Tweets["embedding"].tolist(),labels,test_size)
+            st.write("Data Splitted")
+            model,criterion,optimizer = c.BuildModel(768,labels_n)
+            st.write("Model Built")
+            num_epochs = st.number_input("Epochs", min_value=10, max_value=50, step=5)
+            model = c.TrainModel(model,criterion,optimizer,num_epochs, train_loader)
+            st.write("Model Trained")
+            st.write("Result of Testing Model:", c.TestModel(model,test_loader))
