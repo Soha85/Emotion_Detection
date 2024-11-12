ee77486865e84a3c113e6bc77ee967692fc13331
Soha <sohaelsafey@yahoo.com>
2024-11-12 22:14:14 +0200

    Building, Training & Testing Commit

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ with col2:
     st.write("**Bert + CNN Model**")
     test_size = st.number_input("Test Size", min_value=0.1, max_value=0.5, step=0.1)
     if st.button("Split Data") and st.session_state.tweets_embeddings is not None:
-        print(type(st.session_state.tweets_embeddings.values))
+        print(type(st.session_state.tweets_embeddings))
         train_loader, test_loader, labels_n = c.TrainPreparing(
             st.session_state.tweets_embeddings, st.session_state.tweets[st.session_state.labels].values, test_size)
     # try:
