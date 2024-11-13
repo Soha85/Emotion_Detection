ac95cc960b8db183746a7d492d65ea2ac21100ce
Soha <sohaelsafey@yahoo.com>
2024-11-13 19:33:50 +0200

    Building, Training & Testing Commit

--- TweetCNN.py
+++ TweetCNN.py
@@ -10,8 +10,8 @@ class TweetCNN(nn.Module):
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=128, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool1d(kernel_size=2)
 
-        self.conv2 = nn.Conv1d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.pool2 = nn.MaxPool1d(kernel_size=2)
+        # self.conv2 = nn.Conv1d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
+        # self.pool2 = nn.MaxPool1d(kernel_size=2)
 
         self.dropout = nn.Dropout(0.5)
 
@@ -30,8 +30,8 @@ class TweetCNN(nn.Module):
         x = torch.relu(self.conv1(x))
         x = self.pool1(x)
 
-        x = torch.relu(self.conv2(x))
-        x = self.pool2(x)
+        # x = torch.relu(self.conv2(x))
+        # x = self.pool2(x)
 
         x = self.dropout(x)  # Apply dropout after pooling
 
