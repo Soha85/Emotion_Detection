1e9c05356cb683d2f079816ceb7fe1618dcbeaf9
Soha <sohaelsafey@yahoo.com>
2024-11-16 21:27:43 +0200

    Building, Training & Testing Commit

--- TweetCNN.py
+++ TweetCNN.py
@@ -7,13 +7,13 @@ class TweetCNN(nn.Module):
         super(TweetCNN, self).__init__()
 
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=128, kernel_size=5, padding=1)
-        self.pool1 = nn.MaxPool1d(kernel_size=3, stride=1)
+        self.pool1 = nn.MaxPool1d(kernel_size=3, stride=2)
 
-        self.conv2 = nn.Conv1d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=1)
+        self.conv2 = nn.Conv1d(in_channels=128, out_channels=64, kernel_size=5, padding=1)
+        self.pool2 = nn.MaxPool1d(kernel_size=3, stride=2)
 
         # Adjusted fully connected layer input size to match flattened size
-        self.fc = nn.Linear(64 * 192, num_classes)  # Output size matches number of classes (11)
+        self.fc = nn.Linear(64 * 191, num_classes)  # Output size matches number of classes (11)
         self.dropout = nn.Dropout(0.1)
 
     # Ensure the forward method is correctly defined here
